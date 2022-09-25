44d38ad77b57cb607e970d34694422582ec038a1
samjwu <sjwu@ualberta.ca>
2022-09-25 10:38:04 -0600

    track specialty classes

--- res/1races.js
+++ res/1races.js
@@ -1,40 +1,47 @@
 window.mammal = {
+    class: "mammal",
     atk: 2,
     def: 2,
     spd: 2
 };
 
 window.bird = {
+    class: "bird",
     atk: 2,
     def: 1,
     spd: 3
 };
 
 window.reptile = {
+    class: "reptile",
     atk: 3,
     def: 2,
     spd: 1
 };
 
 window.amphibian = {
+    class: "amphibian",
     atk: 1,
     def: 2,
     spd: 3
 };
 
 window.fish = {
+    class: "fish",
     atk: 2,
     def: 3,
     spd: 1
 };
 
 window.insect = {
+    class: "insect",
     atk: 1,
     def: 3,
     spd: 2
 };
 
 window.mollusk = {
+    class: "mollusk",
     atk: 3,
     def: 2,
     spd: 1
