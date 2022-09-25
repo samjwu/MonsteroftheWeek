0627d136e52664da59cf7179a9d996f0a0bc5969
samjwu <sjwu@ualberta.ca>
2022-09-25 17:53:52 -0600

    describe basic mammal abilities

--- res/2abilities.js
+++ res/2abilities.js
@@ -6,3 +6,21 @@ window.abilityMap = new Map([
     ["jumping", "Puma pounce"],
     ["rabies", "Mammal rabies"],
 ]);
+
+setup.restrictAbilities = function () {
+    $(document).on(':passagedisplay', function () {
+        var maxAbilities = 2;
+        function isValidSelection () {
+            var checkedChecks = document.querySelectorAll("input:checked");
+            if (checkedChecks.length >= maxAbilities + 1) {
+                return false;
+            }
+            return true;
+        }
+
+        var checks = document.querySelectorAll("input");
+        for (var i = 0; i < checks.length; i++) {
+            checks[i].onclick = isValidSelection;
+        }
+    });
+}
