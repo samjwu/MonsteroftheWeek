706c4abd7084acc64c10f76d827c2482d546ebc3
samjwu <sjwu@ualberta.ca>
2022-09-25 10:37:53 -0600

    add mammal abilities

---
+++ res/2abilities.js
@@ -0,0 +1,8 @@
+window.abilityMap = new Map([
+    ["muscles", "Gorilla grip"],
+    ["blubber", "Hippo hide"],
+    ["running", "Cheetah speed"],
+    ["eyesight", "Eagle eye"],
+    ["jumping", "Puma pounce"],
+    ["rabies", "Mammal rabies"],
+]);
