da6e1332665b4bf9f73444cba8925dfe4c07bb12
samjwu <sjwu@ualberta.ca>
2022-09-27 18:13:54 -0600

    add bird abilities

--- res/2abilities.js
+++ res/2abilities.js
@@ -2,9 +2,10 @@ window.abilityMap = new Map([
     ["muscles", "Gorilla grip"],
     ["blubber", "Hippo hide"],
     ["running", "Cheetah speed"],
-    ["eyesight", "Eagle eye"],
     ["jumping", "Puma pounce"],
     ["rabies", "Mammal rabies"],
+    
+    ["eyesight", "Eagle eye"],
 ]);
 
 setup.restrictAbilities = function () {
