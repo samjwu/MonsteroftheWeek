316b51628a5cf8e1eb0fdc73ba790ccfb97c231d
samjwu <sjwu@ualberta.ca>
2022-10-08 19:44:40 -0600

    random enemy names

--- res/3enemies.js
+++ res/3enemies.js
@@ -1,3 +1,17 @@
+var adjectives = new Array(
+    "monster",
+    "monstrous",
+    "terrifying",
+    "horrifying",
+    "dreadful",
+    "ghastly",
+    "horrible",
+    "beastly",
+    "scary",
+    "creepy",
+    "spooky"
+);
+
 function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -5,7 +19,7 @@ function getRandomIntInclusive(min, max) {
 setup.generateEnemy = function(playerFame, enemyRace) {
     enemy = new Object();
 
-    enemy.name = enemyRace.class;
+    enemy.name = adjectives[getRandomIntInclusive(0, adjectives.length)] + " " + enemyRace.class;
 
     enemy.atk = getRandomIntInclusive(0, playerFame) + enemyRace.atk;
     enemy.def = getRandomIntInclusive(0, playerFame) + enemyRace.def;
