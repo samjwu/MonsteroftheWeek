57cc2fff3589e082026ff05b020475678d3eb533
samjwu <sjwu@ualberta.ca>
2022-10-08 19:50:16 -0600

    generate random enemy

--- res/3enemies.js
+++ res/3enemies.js
@@ -12,24 +12,24 @@ var adjectives = new Array(
     "spooky"
 );
 
-function getRandomIntInclusive(min, max) {
+setup.getRandomIntInclusive = function(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 setup.generateEnemy = function(playerFame, enemyRace) {
     enemy = new Object();
 
-    enemy.name = adjectives[getRandomIntInclusive(0, adjectives.length)] + " " + enemyRace.class;
+    enemy.name = adjectives[setup.getRandomIntInclusive(0, adjectives.length - 1)] + " " + enemyRace.class;
 
-    enemy.atk = getRandomIntInclusive(0, playerFame) + enemyRace.atk;
-    enemy.def = getRandomIntInclusive(0, playerFame) + enemyRace.def;
-    enemy.spd = getRandomIntInclusive(0, playerFame) + enemyRace.spd;
+    enemy.atk = setup.getRandomIntInclusive(0, playerFame) + enemyRace.atk;
+    enemy.def = setup.getRandomIntInclusive(0, playerFame) + enemyRace.def;
+    enemy.spd = setup.getRandomIntInclusive(0, playerFame) + enemyRace.spd;
 
-    enemy.maxStamina = 100 + getRandomIntInclusive(0, playerFame);
-    enemy.staminaDrainPercent = 100 - getRandomIntInclusive(0, playerFame);
+    enemy.maxStamina = 100 + setup.getRandomIntInclusive(0, playerFame);
+    enemy.staminaDrainPercent = 100 - setup.getRandomIntInclusive(0, playerFame);
 
-    enemy.accuracyPercent = 100 + getRandomIntInclusive(0, playerFame);
-    enemy.damageMultiplierPercent = 100 + getRandomIntInclusive(0, playerFame);
+    enemy.accuracyPercent = 100 + setup.getRandomIntInclusive(0, playerFame);
+    enemy.damageMultiplierPercent = 100 + setup.getRandomIntInclusive(0, playerFame);
 
     return enemy;
 }
