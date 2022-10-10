0859efc22902512833645d5b86ed55cecad99556
samjwu <sjwu@ualberta.ca>
2022-10-09 19:15:35 -0600

    add differences to move types

--- res/4combat.js
+++ res/4combat.js
@@ -7,22 +7,22 @@ function calculateMitigation(def) {
     return Math.random() * def * 10;
 }
 
-function determineIfHit(spd, accuracy) {
+function determineIfHit(spd, accuracy, move) {
     var dodgeChance = Math.random() * spd * 10;
-    var hitChance = Math.random() * 100 + (accuracy - 100);
+    var hitChance = Math.random() * 100 + (accuracy - 100) - (move * 10);
     return hitChance >= dodgeChance;
 }
 
 setup.calculateTurn = function(player, enemy, move) {
     var damageDealt = calculateDamage(player.atk, player.damageMultiplierPercent / 100, move);
     var finalDamageDealt = damageDealt - calculateMitigation(enemy.def);
-    if (determineIfHit(enemy.spd, player.accuracyPercent)) {
+    if (determineIfHit(enemy.spd, player.accuracyPercent, move)) {
         enemy.currentStamina -= finalDamageDealt;
     }
 
     var damageTaken  = calculateDamage(enemy.atk, enemy.damageMultiplierPercent / 100, move);
     var finalDamageTaken = damageTaken - calculateMitigation(player.def);
-    if (determineIfHit(player.spd, enemy.accuracyPercent)) {
+    if (determineIfHit(player.spd, enemy.accuracyPercent, 0)) {
         player.currentStamina -= finalDamageTaken;
     }
 }
