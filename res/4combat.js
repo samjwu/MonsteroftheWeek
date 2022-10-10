5fc31097b7899a8b464cce6a19d7eb23996b97a6
samjwu <sjwu@ualberta.ca>
2022-10-09 19:13:20 -0600

    add dodge chance

--- res/4combat.js
+++ res/4combat.js
@@ -7,12 +7,22 @@ function calculateMitigation(def) {
     return Math.random() * def * 10;
 }
 
+function determineIfHit(spd, accuracy) {
+    var dodgeChance = Math.random() * spd * 10;
+    var hitChance = Math.random() * 100 + (accuracy - 100);
+    return hitChance >= dodgeChance;
+}
+
 setup.calculateTurn = function(player, enemy, move) {
     var damageDealt = calculateDamage(player.atk, player.damageMultiplierPercent / 100, move);
     var finalDamageDealt = damageDealt - calculateMitigation(enemy.def);
-    enemy.currentStamina -= finalDamageDealt;
+    if (determineIfHit(enemy.spd, player.accuracyPercent)) {
+        enemy.currentStamina -= finalDamageDealt;
+    }
 
     var damageTaken  = calculateDamage(enemy.atk, enemy.damageMultiplierPercent / 100, move);
     var finalDamageTaken = damageTaken - calculateMitigation(player.def);
-    player.currentStamina -= finalDamageTaken;
+    if (determineIfHit(player.spd, enemy.accuracyPercent)) {
+        player.currentStamina -= finalDamageTaken;
+    }
 }
